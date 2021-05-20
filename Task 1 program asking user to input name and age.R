f4676631a6e7329a569a92d632811de7fc7567ed
Rebecca Uddin <84407796+rebecca-u@users.noreply.github.com>
2021-05-20 16:41:26 +0100

    Add files via upload

---
+++ Task 1 program asking user to input name and age.R
@@ -0,0 +1,11 @@
+#write a program to ask user their name
+name <- readline("Enter your name: ")
+hello <- paste ("Welcome", name)
+#display name as values
+print(hello)
+
+#ask user to input their age
+age <- readline("Enter your age: ")
+age <- paste ("Hi", name, "you are", age, "years old.")
+#display age as a value
+print(age)
