f67bf02bab4e06ec887c783355bd0ff499f50bfe
Rebecca Uddin <84407796+rebecca-u@users.noreply.github.com>
2021-05-20 16:38:14 +0100

    Add files via upload

---
+++ Task 4 Create a data frame of employee details.R
@@ -0,0 +1,8 @@
+Employees = data.frame(Name=c("Andy Brown","Ben Smith","Cathy Lewis", "Dina Khan","Emma Hart"),
+                       Gender=c("M","M","F","F","F"),
+                       Age=c(43,52,35,24,31),
+                       Role=c("Doctor","Porter","Cleaner","Receptionist","Nurse"),
+                       Length.of.service=c("10 years","15 years","6 years","2 year","3 years"))
+print("Details of the employees:")                      
+print(Employees)
+
---
+++ Task 5 Create a simple bar chart of 5 subjects.R
@@ -0,0 +1,9 @@
+marks = c(77, 87, 82, 64, 78)
+barplot(marks,
+        main = "Comparing marks of 5 subjects",
+        xlab = "Marks",
+        ylab = "Subject",
+        names.arg = c("Maths", "Science", "English", "Art", "PE"),
+        col = "blue",
+        horiz = FALSE)
+
